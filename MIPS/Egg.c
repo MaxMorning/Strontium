192cbfbb79170101d3ca4809a14db4e15003d5b1
MaxMorning <hml0814@163.com>
2021-12-11 23:11:12 +0800

    [CODE]Adjust Egg.c
    这实验指导是真的**

--- MIPS/Egg.c
+++ MIPS/Egg.c
@@ -1,107 +1,59 @@
 #include <stdio.h>
 
-__attribute__((always_inline)) int max(int a, int b)
-{
-    return a > b ? a : b;
-}
-
-__attribute__((always_inline)) int min(int a, int b)
-{
-    return a < b ? a : b;
-}
-
 int main()
 {
-    // dp_array[k][n] 代表有k + 1个鸡蛋，测试n层楼的最坏情况（尝试次数）
-    int egg_sum, floor_sum;
-    int** dp_array;
+    int strength_cnt, floor_sum;
 
 #ifdef EMBEDDED
 
     // input from embedded I/O
     // Memory mapping : 
-    // 0x8000 0000  egg_sum
-    // 0x8000 0004  floor_sum
-    // 0x8000 0008  result
-    // 0x0000 0000  addr of dp_array[0]
-    // egg_sum * (floor_sum + 1) * 4 stack base of main
+    // 0x8000 0000  in  strength_cnt
+    // 0x8000 0004  in  floor_sum
+    // 0x8000 0008  out toss_cnt
+    // 0x8000 000c  out egg_cnt 
+    // 0x8000 0010  out is_egg_break
     __asm__ (
         "lui $t0, 0x8000\t\n" // t0 is the base addr of I/O
         "addi $t1, $0, 0xffff\t\n"
-        "sw $t1, 0x8($t0)\t\n" // display ffff when calc not done
-        "lw %[egg_sum_reg], 0($t0)\t\n"
+        "sw $t1, 0x10($t0)\t\n" // LED display ffff when calc not done
+        "lw %[strength_cnt_reg], 0($t0)\t\n"
         "lw %[floor_sum_reg], 4($t0)\t\n"
-        // "addi %[dp_array_reg], $0, 0xc\t\n"
-
-        // initial memory from 0x0 to 0x0 + (egg_sum - 1) * 4
-        "addi $t1, $0, 0\t\n" // $t1 is &dp_array[i]
-        "sll $t3, %[egg_sum_reg], 2\t\n" // $t3 is egg_sum_reg * 4
-        "sll $t4, %[floor_sum_reg], 2\t\n"
-        "addi $t4, $t4, 0x4\t\n" // $t4 is sizeof(dp_array[i])
-        "addi $t2, $t3, 0x0\t\n" // $t2 is dp_array[i]
-        "COMPARE:\t\n"
-        "beq $t1, $t3, FINISH_ASM\t\n"
-        // "nop\t\n" delay slot will be added by compiler
-        "sw $t2, 0x0($t1)\t\n"
-        "addiu $t1, $t1, 0x4\t\n"
-        "addu $t2, $t2, $t4\t\n"
-        "j COMPARE\t\n"
-        // "nop\t\n" delay slot will be added by compiler
+        
         "FINISH_ASM:\t\n"
         "addi $s8, $t2, 0x0\t\n" // set s8 as stack pointer (GCC likes it)
         : // output
-        [egg_sum_reg]"=r"(egg_sum),
+        [strength_cnt_reg]"=r"(strength_cnt),
         [floor_sum_reg]"=r"(floor_sum)
         : // input
         :
-        "memory", "$t0", "$t1", "$t2", "$t3", "$t4" // destroy
+        "memory", "$t0", "$t1" // destroy
     );
 
-    dp_array = 0;
 #else
-    scanf("%d %d", &egg_sum, &floor_sum);
-
-    dp_array = new int*[egg_sum];
-    for (int i = 0; i < egg_sum; ++i) {
-        dp_array[i] = new int[floor_sum + 1];
-    }
+    scanf("%d %d", &strength_cnt, &floor_sum);
 
 #endif
+    register int toss_cnt = 0;
+    register int egg_cnt = 0;
+    register int is_egg_break = 0;
+    //  binary search
+    register int left = 0;
+    register int right = floor_sum;
 
-    // DP start
-    for (register int i = 0; i <= floor_sum; ++i) {
-        dp_array[0][i] = i;
-    }
 
-    for (register int i = 0; i < egg_sum; ++i) {
-        dp_array[i][0] = 0;
-    }
+    while (left < right) {
+        register int middle = (left + right) >> 1;
+        ++toss_cnt;
 
-    for (register int egg_idx = 1; egg_idx < egg_sum; ++egg_idx) {
-        for (register int floor_idx = 1; floor_idx <= floor_sum; ++floor_idx) {
-
-            register int lo = 1;
-            register int hi = floor_idx;
-
-            while (lo + 1 < hi) {
-                register int middle = (lo + hi) / 2;
-                register int t1 = dp_array[egg_idx - 1][middle - 1];
-                register int t2 = dp_array[egg_idx][floor_idx - middle];
-
-                if (t1 < t2) {
-                    lo = middle;
-                }
-                else if (t1 > t2) {
-                    hi = middle;
-                }
-                else {
-                    lo = middle;
-                    hi = middle;
-                }
-            }
-
-            dp_array[egg_idx][floor_idx] = 1 + min(max(dp_array[egg_idx - 1][lo - 1], dp_array[egg_idx][floor_idx - lo]),
-                                                   max(dp_array[egg_idx - 1][hi - 1], dp_array[egg_idx][floor_idx - hi]));
+        if (middle <= strength_cnt) {
+            right = middle;
+            is_egg_break = 0;
+        }
+        else {
+            left = middle + 1;
+            ++egg_cnt;
+            is_egg_break = 1;
         }
     }
 
@@ -109,16 +61,20 @@ int main()
     // output to I/O
     __asm__ volatile (
         "lui $t0, 0x8000\t\n" // t0 is the base addr of I/O
-        "sw %[result], 8($t0)\t\n"
+        "sw %[toss_cnt_reg], 8($t0)\t\n"
+        "sw %[egg_cnt_reg], 0xc($t0)\t\n"
+        "sw %[is_egg_break_reg], 0x10($t0)\t\n"
         "END_TRAP:\t\n"
         "j END_TRAP\t\n"
         :
         : // input
-        [result]"r"(dp_array[egg_sum - 1][floor_sum])
+        [toss_cnt_reg]"r"(toss_cnt),
+        [egg_cnt_reg]"r"(egg_cnt),
+        [is_egg_break_reg]"r"(is_egg_break)
         : "$t0" // destroy
     );
 #else
-    printf("%d\n", dp_array[egg_sum - 1][floor_sum]);
+    printf("Toss: %d, Egg: %d, IsBreak: %d\n", toss_cnt, egg_cnt, is_egg_break);
 #endif
 
     return 0;
